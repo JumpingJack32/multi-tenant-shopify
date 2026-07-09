cfc979779e65dab06674220cc0c857336f0511b3
JumpingJack32 <jumpingjackboxx@gmail.com>
2026-07-09 12:58:05 +0100

    fix: belt-and-suspenders next-env.d.ts override in app eslint configs

--- apps/storefront/eslint.config.js
+++ apps/storefront/eslint.config.js
@@ -1,3 +1,14 @@
 import config from "@repo/eslint-config";
 
-export default [{ ignores: ["**/next-env.d.ts"] }, ...config];
+export default [
+  {
+    ignores: ["**/next-env.d.ts"],
+  },
+  {
+    files: ["**/next-env.d.ts"],
+    rules: {
+      "import/no-unresolved": "off",
+    },
+  },
+  ...config,
+];
